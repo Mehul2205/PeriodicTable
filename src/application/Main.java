e2466ba180dbaade765a5e29c570bd6405c3f600
Mehul Patni <mehulpatni.2205@gmail.com>
2019-09-29 00:36:49 +0530

    Init Commit

---
+++ src/application/Main.java
@@ -0,0 +1,41 @@
+package application;
+/*	
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+*/
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,1300,800);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/SampleController.java
@@ -0,0 +1,72 @@
+package application;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextArea;
+
+public class SampleController {
+
+	private static final String FILE_NAME = "PT1.xlsx";
+    
+    @FXML
+    private TextArea outputText;
+	
+	
+    @FXML
+    private void numChange(ActionEvent event) {
+        Node node = (Node) event.getSource() ;
+        String data = (String) node.getUserData();
+        int value = Integer.parseInt(data);
+        int i =0;
+		try {
+			
+            FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
+            Workbook workbook = new XSSFWorkbook(excelFile);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+            	Row current1 = datatypeSheet.getRow(0);
+            	
+                Row currentRow = datatypeSheet.getRow(value);
+                Iterator<Cell> cellIterator = currentRow.iterator();
+                Iterator<Cell> cellIterator2 = current1.iterator();
+                outputText.setText("");
+                while (cellIterator2.hasNext()) {
+
+                    Cell currentCell = cellIterator.next();
+                    
+                    Cell c1 = cellIterator2.next();
+                    if (currentCell.getCellType() == Cell.CELL_TYPE_STRING) {
+                    	outputText.appendText(c1.getStringCellValue()+" = "+currentCell.getStringCellValue()+"\t\t");
+                    } else if (currentCell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+                    	outputText.appendText(c1.getStringCellValue()+" = "+currentCell.getNumericCellValue()+"\t\t");
+                    } else {
+                    	continue;
+                    }
+                    /*
+                    i++;
+                    if(i%2==0) {
+                    	outputText.appendText("\n");
+                    }*/
+                    outputText.appendText("\n");
+                }
+                
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
